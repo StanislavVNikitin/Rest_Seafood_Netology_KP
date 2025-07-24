7f9d2f420c01bf0be5c1096e1e5e29c98a1a509d
Fox587 <fox587@bk.ru>
2025-07-24 12:24:13 +0000

    Add .gitignore for Bitrix project, update favicon permissions, modify index.php and contact.php for improved structure, and enhance urlrewrite.php with new rules.

--- about/index.php
+++ about/index.php
@@ -1,4 +1,69 @@
-<?
+<? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+\Bitrix\Main\UI\Extension::load(["ui.bootstrap4", "ui.fonts.opensans"]);
+
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("О нас");
-?>О ресторане<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+$APPLICATION->SetTitle("О ресторане");
+?>
+<header>
+<?
+$APPLICATION->IncludeComponent(
+    "bitrix:menu",
+    "",
+    array(
+        "ROOT_MENU_TYPE" => "top",
+        "MENU_CACHE_TYPE" => "A",
+        "MENU_CACHE_TIME" => "3600",
+        "MENU_CACHE_USE_GROUPS" => "Y",
+        "MENU_CACHE_GET_VARS" => array(),
+        "MAX_LEVEL" => "1",
+        "CHILD_MENU_TYPE" => "",
+        "USE_EXT" => "N",
+        "DELAY" => "N",
+        "ALLOW_MULTI_SELECT" => "N"
+    ),
+    false
+);
+?>
+</header>
+<div class="bradcam_area bradcam_bg_2">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12">
+                <div class="bradcam_text text-center">
+                    <h3>О нас</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--/ bradcam_area  -->
+
+<div class="main_about_wrap">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12">
+                <h3>Exercitation photo booth stumptown tote
+                    bag todo Banksy elit small</h3>
+                    <p>Omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi. Exercitation photo booth stumptown tote bag todo Banksy, elit small batch freegan sed. Craft beer elit seitan exercitation photo booth et 8-bit kale chips proident chillwave deep vow laborum. Aliquip veniam delectus, marfa eiusmod pinterest.</p>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-lg-10">
+                <div class="row">
+                    <div class="col-xl-6 col-md-6">
+                        <div class="about_thumb">
+                            <img src="img/about/about_1.png" alt="">
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-md-6">
+                        <div class="about_thumb">
+                            <img src="img/about/about_2.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
--- urlrewrite.php
+++ urlrewrite.php
@@ -1,5 +1,13 @@
 <?php
 $arUrlRewrite=array (
+  3 => 
+  array (
+    'CONDITION' => '#^\\/?\\/mobileapp/jn\\/(.*)\\/.*#',
+    'RULE' => 'componentName=$1',
+    'ID' => NULL,
+    'PATH' => '/bitrix/services/mobileapp/jn.php',
+    'SORT' => 100,
+  ),
   0 => 
   array (
     'CONDITION' => '#^/services/#',
@@ -22,6 +30,14 @@ $arUrlRewrite=array (
     'RULE' => '',
     'ID' => 'bitrix:news',
     'PATH' => '/news/index.php',
+    'SORT' => 100,
+  ),
+  4 => 
+  array (
+    'CONDITION' => '#^/rest/#',
+    'RULE' => '',
+    'ID' => NULL,
+    'PATH' => '/bitrix/services/rest/index.php',
     'SORT' => 100,
   ),
 );
