8a0ec82495481a4b851df3988c7fc294c332c7d2
Fox587 <fox587@bk.ru>
2025-07-25 08:45:38 +0000

    Refactor about page and header template; remove unused menu component, update CHILD_MENU_TYPE, and add new image assets for improved design and functionality.

--- about/index.php
+++ about/index.php
@@ -6,30 +6,7 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
 $APPLICATION->SetTitle("О ресторане");
 ?>
-<header>
-<?
-$APPLICATION->IncludeComponent(
-	"bitrix:menu", 
-	"horizontal_multilevel", 
-	array(
-		"ROOT_MENU_TYPE" => "top",
-		"MENU_CACHE_TYPE" => "A",
-		"MENU_CACHE_TIME" => "3600",
-		"MENU_CACHE_USE_GROUPS" => "Y",
-		"MENU_CACHE_GET_VARS" => array(
-		),
-		"MAX_LEVEL" => "1",
-		"CHILD_MENU_TYPE" => "top",
-		"USE_EXT" => "N",
-		"DELAY" => "N",
-		"ALLOW_MULTI_SELECT" => "N",
-		"COMPONENT_TEMPLATE" => "horizontal_multilevel"
-	),
-	false
-);
-?>
-</header>
-<div class="bradcam_area bradcam_bg_2">
+<div class="bradcam_area bradcam_bg_В2">
     <div class="container">
         <div class="row">
             <div class="col-xl-12">
