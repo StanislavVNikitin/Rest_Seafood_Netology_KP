11e78bdb50284ab65f9e4cdb0cb47ebb1916a78c
Fox587 <fox587@bk.ru>
2025-07-31 15:50:42 +0000

    Swap URL rewrite rules for catalog and reviews, updating paths accordingly. Modify catalog index page to disable title setting for improved SEO handling.

--- urlrewrite.php
+++ urlrewrite.php
@@ -24,20 +24,20 @@ $arUrlRewrite=array (
     'PATH' => '/products/index.php',
     'SORT' => 100,
   ),
-  5 => 
-  array (
-    'CONDITION' => '#^/catalog/#',
-    'RULE' => '',
-    'ID' => 'bitrix:news',
-    'PATH' => '/catalog/index.php',
-    'SORT' => 100,
-  ),
   6 => 
   array (
     'CONDITION' => '#^/reviews/#',
     'RULE' => '',
     'ID' => 'bitrix:news',
     'PATH' => '/reviews/index.php',
+    'SORT' => 100,
+  ),
+  7 => 
+  array (
+    'CONDITION' => '#^/catalog/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/catalog/index.php',
     'SORT' => 100,
   ),
   2 => 
