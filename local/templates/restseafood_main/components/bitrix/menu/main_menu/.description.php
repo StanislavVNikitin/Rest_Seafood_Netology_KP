a74e055502faa00d3827cb8f598b9781df12a5c8
StanislavVNikitin <stasvnikitin@mail.ru>
2025-08-01 22:18:18 +0300

    removing unused components

--- local/templates/restseafood_main/components/bitrix/menu/main_menu/.description.php
+++
@@ -1,8 +0,0 @@
-<?
-if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
-
-$arTemplateDescription = array(
-	"NAME" => 'Учебный шаблон меню',
-	"DESCRIPTION" => 'Учебный шаблон меню',
-);
-?>
