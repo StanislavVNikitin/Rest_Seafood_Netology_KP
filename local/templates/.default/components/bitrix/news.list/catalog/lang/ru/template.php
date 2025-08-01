73561698e3ebfa7aa2b10d1b2b77d03595f81634
Fox587 <fox587@bk.ru>
2025-08-01 18:46:40 +0000

    Remove outdated language files for the news list component in both English and Russian, streamlining the template structure and eliminating unused translations.

--- local/templates/.default/components/bitrix/news.list/catalog/lang/ru/template.php
+++
@@ -1,4 +0,0 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-$MESS["CT_BNL_ELEMENT_DELETE_CONFIRM"] = "Будет удалена вся информация, связанная с этой записью. Продолжить?";
-
-?>
